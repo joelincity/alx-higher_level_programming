736e6074808e322271a363f0592e45539e454d23
root <dadonye@yahoo.com>
2023-02-01 06:24:18 -0800

    updated

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""Printing square"""
+# 4-print_square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """Prints a square with the character '#'"""
-    if type(size) != int:
+    """Print a square with the # character.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+        """
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for x in range(size):
-        for y in range(size):
-            print("#", end='')
-            print()
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
