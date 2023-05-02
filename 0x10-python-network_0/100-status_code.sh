891cccc209e9067ec51968561b6d14f365f65316
root <dadonye@yahoo.com>
2023-05-02 05:12:30 -0700

    updated

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Displays only the status code
+curl -sLw "%{http_code}" -o /dev/null "$1"
